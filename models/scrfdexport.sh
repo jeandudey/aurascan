a585dffa61d7f0f80f8c42d7e8604aa2157ff7d2
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2026-06-10 15:28:09 +0200

    Add SCRFD modelinfo

--- models/scrfdexport.sh
+++ models/scrfdexport.sh
@@ -14,6 +14,7 @@ scrfd2onnx scrfd_1g SCRFD_1G
 scrfd2onnx scrfd_2.5g SCRFD_2_5G
 scrfd2onnx scrfd_10g SCRFD_10G
 scrfd2onnx scrfd_34g SCRFD_34G
+scrfd2onnx scrfd_500m SCRFD_500M
 scrfd2onnx scrfd_500m_bnkps SCRFD_500M_KPS
 scrfd2onnx scrfd_2.5g_bnkps SCRFD_2_5G_KPS
 scrfd2onnx scrfd_10g_bnkps SCRFD_10G_KPS

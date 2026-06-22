ca69aa27b74b16dbc77aca7a4f34b48a67c6665e
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2026-06-22 14:33:55 +0200

    Remove old css.

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,10 @@ use crate::app::AppModel;
 use crate::application::Application;
 use relm4::RelmApp;
 
-const CSS: &str = include_str!("app.css");
-
 fn main() {
     let application = Application::new();
 
     let app = RelmApp::from_app(application);
-    //relm4::set_global_css(CSS);
     app.run::<AppModel>(());
     unsafe { gst::deinit() };
 }
